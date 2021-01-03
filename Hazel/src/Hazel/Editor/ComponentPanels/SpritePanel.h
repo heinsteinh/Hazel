c584b20c46ac5177124d067be4e27af29931a363
Adrien Fleury <adrien_le_hardi@hotmail.fr>
2021-01-03 17:09:37 +0100

    Minor changes

--- Hazel/src/Hazel/Editor/ComponentPanels/SpritePanel.h
+++ Hazel/src/Hazel/Editor/ComponentPanels/SpritePanel.h
@@ -21,32 +21,43 @@ namespace Hazel
 			ImGui::ColorEdit4("Color", glm::value_ptr(component.Color));
 			if (ImGui::TreeNodeEx("Texture", TreeNodeFlags::GetDefaultFlags()))
 			{
-				ImGui::Text("Source: %s", component.TextureFilename.c_str());
-				if (ImGui::Button("Remove"))
-				{
-					component.TextureFilename.clear();
-					component.Texture.SetSource(nullptr);
-				}
-				ImGui::SameLine();
-				if (ImGui::Button("Browse..."))
-				{
-					FileDialog dialog(entity.GetLayer().GetWindow());
-					if (dialog.GetOpenFilename())
-					{
-						component.TextureFilename = dialog.GetFilename();
-						component.Texture = entity.GetTextureManager().Load(component.TextureFilename);
-					}
-				}
-				if (component.Texture)
-				{
-					ImGui::Text("Region");
-					auto region = component.Texture.GetRegion();
-					if (rectanglePanel.Draw(region))
-					{
-						component.Texture.SetRegion(region);
-					}
-				}
+				DrawTextureSource(entity, component);
+				DrawTextureRegion(component);
 				ImGui::TreePop();
+			}
+		}
+
+	private:
+		void DrawTextureSource(Entity entity, SpriteComponent &component)
+		{
+			ImGui::Text("Source: %s", component.TextureFilename.empty() ? "None" : component.TextureFilename.c_str());
+			if (ImGui::Button("Remove"))
+			{
+				component.TextureFilename.clear();
+				component.Texture.SetSource(nullptr);
+			}
+			ImGui::SameLine();
+			if (ImGui::Button("Browse..."))
+			{
+				FileDialog dialog(entity.GetLayer().GetWindow());
+				if (dialog.GetOpenFilename())
+				{
+					component.TextureFilename = dialog.GetFilename();
+					component.Texture = entity.GetTextureManager().Load(component.TextureFilename);
+				}
+			}
+		}
+
+		void DrawTextureRegion(SpriteComponent &component)
+		{
+			if (component.Texture)
+			{
+				ImGui::Text("Region");
+				auto region = component.Texture.GetRegion();
+				if (rectanglePanel.Draw(region))
+				{
+					component.Texture.SetRegion(region);
+				}
 			}
 		}
 	};
